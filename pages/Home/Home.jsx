b28e8e1a486c99170f015a74abd177577b363eb1
Donna Hope B. Banzuela <donna.banzuela@asialinkfinance.com.ph>
2026-05-04 23:06:35 +0800

    fix

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -11,7 +11,7 @@ import Footer from "../../components/Footer/Footer";
 const Home = () => {
   return (
     <>
-      {/* <HeroSection/> */}
+      <HeroSection/>
       {/* <AboutSection/> */}
       <ServicesSection/>
       {/* <ProjectsSection/>
