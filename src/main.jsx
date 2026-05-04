1e59d4ea1a852f697586c433dd6558bb3d56ee9d
Donna Hope B. Banzuela <donna.banzuela@asialinkfinance.com.ph>
2026-05-04 10:34:00 +0800

    change main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Portfolio2026"> 
+    <BrowserRouter> 
       <App />
     </BrowserRouter>
   </React.StrictMode>,
