5bf49d16a13ed5ff2178234754211d440af9b458
Donna Hope B. Banzuela <donna.banzuela@asialinkfinance.com.ph>
2026-05-04 10:42:02 +0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect} from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { initRevealAnimation, watchForNewRevealElements, destroyRevealAnimation, refreshRevealAnimation } from "../utils/scrollAnimation.js";import Home from '../pages/Home/Home.jsx';
 import MainLayout from "../layout/MainLayout";
+import '../src/index.css'
 
 
 function App() {
